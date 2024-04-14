6d3b445d9d64fd867f117babf45d248443ccae18
nhan.trinh <hongnhan50@gmail.com>
2024-04-14 18:55:28 +0700

    Feature: implemented animation for run, jump , fall

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,11 +10,27 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 		if velocity.y > 500:
 			velocity.y = 500
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump") && is_on_floor():
 		velocity.y = -jump_force
 	
 	var direction = Input.get_axis("move_left" ,"move_right")
+	
+	if direction !=0 :
+		animated_sprite.flip_h = (direction == -1 )
 
 	velocity.x = direction * speed
 	move_and_slide()
+	update_animations(direction)
 	
+func update_animations(direction):
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("Idle")
+		else:
+			animated_sprite.play("Run")
+	else: # player is not on floor
+		if velocity.y < 0:
+			animated_sprite.play("Jump")
+		else:
+			animated_sprite.play("Fall")
+			
