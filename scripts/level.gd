4f3b67d8e8f9c71721a4d7b66b0fa04679ef7b52
nhan.trinh <hongnhan50@gmail.com>
2024-04-20 19:52:41 +0700

    Rewrite for connected touched_player _on_trap_touched_player

--- scripts/level.gd
+++ scripts/level.gd
@@ -9,14 +9,16 @@ func _process(delta):
 	elif Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
 
+func _ready():
+	var traps = get_tree().get_nodes_in_group("traps")
+	for trap in traps:
+		trap.connect("touched_player", _on_trap_touched_player)
 
 func _on_deathzone_body_entered(body):
 	reset_player()
 
-
 func _on_trap_touched_player():
 	reset_player()
-
 
 func reset_player():
 	player.velocity = Vector2.ZERO
